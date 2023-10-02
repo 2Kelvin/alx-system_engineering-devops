762dd24c4e2a064e5669e4b919375a2608dba082
2Kelvin <rocketmancodes02@gmail.com>
2023-10-02 12:42:23 -0700

    puppet code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,14 @@
 # Puppet code for http header
 
-exec {'running code in shell':
-    command => 'sudo apt -y update;
-                sudo apt -y install nginx;
-                echo 'Hello World!' > /var/www/html/index.html;
-                sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www\
-                .youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default;
-                echo "Ceci n'est pas une page" > /var/www/html/404.html;
-                sed -i "0,/}/ s|}|}\n\n\terror_page 404 /404.html;\n\tlocation = /404.html \
-                {\n\t\troot /var/www/html/;\n\t\tinternal;\n\t}|" /etc/nginx/sites-available/default;
-                sed -i "/server_name _;/ a\\\tadd_header X-Served-By '$HOSTNAME' always;\n" \
-                /etc/nginx/sites-available/default;
-                sudo service nginx restart',
-    provider => shell
+exec { 'run my shell code':
+  command  => 'sudo apt -y update;
+               sudo apt -y upgrade;
+               sudo apt -y install nginx;
+               sudo /var/www/html/index.nginx-debian.html;
+               echo "Hello World" >> /var/www/html/index.nginx-debian.html;
+               /etc/nginx/sites-enabled/default/sudo sed -i /server_name _;/ a \\trewrite\
+             ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+               sudo sed -i "11i \\\t add_header X-Served-By $HOSTNAME always;\n" /etc/nginx/nginx.conf;
+               sudo service nginx restart',
+  provider => shell,
 }

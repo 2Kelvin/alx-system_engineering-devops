69e65144578774e59e12ca8d295988aea45fb798
2Kelvin <rocketmancodes02@gmail.com>
2023-10-02 12:46:45 -0700

    run my shell code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,8 +4,8 @@ exec { 'run my shell code':
   command  => 'sudo apt -y update;
                sudo apt -y upgrade;
                sudo apt -y install nginx;
-               sudo /var/www/html/index.nginx-debian.html;
-               echo "Hello World" >> /var/www/html/index.nginx-debian.html;
+               sudo /var/www/html/index.html;
+               echo "Hello World" >> /var/www/html/index.html;
                /etc/nginx/sites-enabled/default/sudo sed -i /server_name _;/ a \\trewrite\
              ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
                sudo sed -i "11i \\\t add_header X-Served-By $HOSTNAME always;\n" /etc/nginx/nginx.conf;

d462e3af2a9a8756788a964be5743fd0e95b83ea
2Kelvin <rocketmancodes02@gmail.com>
2023-10-23 08:37:41 -0700

    am coming for you apis :racehorse:

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module gather data from an API"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    EMPLOYEE_NAME = ''
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    TASK_TITLE = []
+    employeeId = int(argv[1])
+
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    for userDict in users.json():
+        if userDict['id'] == employeeId:
+            EMPLOYEE_NAME = userDict.get('name')
+            break
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    for todo in todos.json():
+        if todo['userId'] == employeeId:
+            if todo['completed'] is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(todo.get('title'))
+            TOTAL_NUMBER_OF_TASKS += 1
+
+    firstLine = f'Employee {EMPLOYEE_NAME} is done with'
+    secondLine = f'tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):'
+    print(f'{firstLine} {secondLine}')
+    for title in TASK_TITLE:
+        print(f'     {title}')
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+ALX tasks on the awesome `apis`

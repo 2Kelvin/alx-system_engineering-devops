80615838c0864b9967f4ad08689f1a698137bbea
2Kelvin <rocketmancodes02@gmail.com>
2023-11-08 02:20:40 +0300

    add missing file to fix wordpress issue

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,5 @@
 #  Using strace and tmux to fix wordpress site error
 file { '/var/www/html/wp-includes/class-wp-locale.phpp':
     ensure => 'present',
-    owner  => 'apache2',
-    group  => 'apache2',
-    mode   => '0644',
-    source => 'puppet:///modules/wordpress/locale/class-wp-locale.phpp'
-}
-service { 'apache2':
-    ensure => running
+    source => '/var/www/html/wp-includes/class-wp-locale.phpp'
 }

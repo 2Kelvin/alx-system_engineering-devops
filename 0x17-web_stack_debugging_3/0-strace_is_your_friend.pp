e4bce282aa59bb9d165f409452b139a55872acdc
2Kelvin <rocketmancodes02@gmail.com>
2023-11-08 17:07:20 +0300

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # Using strace and tmux to fix wordpress site error
-$correct_fname = '/var/www/html/wp-settings.php'
-exec { 'fix file'
-    command => "sed -i 's/phpp/php/g' ${correct_fname}",
-    path    => ['/bin', '/usr/bin']
+exec { 'fix file':
+    command  => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+    provider => shell,
 }

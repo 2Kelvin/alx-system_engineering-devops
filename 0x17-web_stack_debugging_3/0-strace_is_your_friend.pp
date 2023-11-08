7c6a7b2027877ea93255ba52ab10fbb1a5f4ea25
2Kelvin <rocketmancodes02@gmail.com>
2023-11-08 16:56:54 +0300

    fix wp errs

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 #  Using strace and tmux to fix wordpress site error
-file { '/var/www/html/wp-includes/class-wp-locale.phpp':
-    ensure => 'present',
-    source => '/var/www/html/wp-includes/class-wp-locale.phpp'
+$correct_fname = '/var/www/html/wp-settings.php'
+exec { 'fix file'
+    command => "sed -i 's/phpp/php/g' ${correct_fname}",
+    path    => ['/bin', '/usr/bin']
 }

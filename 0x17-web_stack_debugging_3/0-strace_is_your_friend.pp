b73b60fc3e867b84130b2a0687ae9c7f6ba4ddbf
2Kelvin <rocketmancodes02@gmail.com>
2023-11-08 16:59:07 +0300

    fix whitespace err

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-#  Using strace and tmux to fix wordpress site error
-$correct_fname = '/var/www/html/wp-settings.php'
-exec { 'fix file'
-    command => "sed -i 's/phpp/php/g' ${correct_fname}",
-    path    => ['/bin', '/usr/bin']
+# Using strace and tmux to fix wordpress site error
+file { '/var/www/html/wp-includes/class-wp-locale.phpp':
+    ensure => 'present',
+    source => '/var/www/html/wp-includes/class-wp-locale.phpp'
 }

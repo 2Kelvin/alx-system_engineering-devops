c9d673960ad55b74e719a97292d38c15d11c27fc
2Kelvin <rocketmancodes02@gmail.com>
2023-11-16 10:34:11 +0300

    fix #files

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # Still increasing file open numbers
 exec { 'increase hard file numbers':
     provider => shell,
-    command  => "sudo sed -i 's/nofile 5/nofile 50000/' /etc/security/limits.conf",
+    command  => "sudo sed -i 's/nofile 5/nofile 65536/' /etc/security/limits.conf",
     before   => Exec['fileTwo']
 }
 exec { 'fileTwo':
     provider => shell,
-    command  => "sudo sed -i 's/nofile 4/nofile 40000/' /etc/security/limits.conf",
+    command  => "sudo sed -i 's/nofile 4/nofile 65536/' /etc/security/limits.conf",
 }
